618eee9ea5b2a5873abd059c7e12851cff8c03c9
Arming Huang <arming@buildthedot.com>
2015-01-09 15:23:52 +0700

    Add class Atm and Test run

---
+++ src/atm/Atm.java
@@ -0,0 +1,17 @@
+package atm;
+
+public class Atm {
+	
+	private double money = 0.0;
+	
+	public Atm(double cash){
+		this.money = cash;
+	}
+	
+	public int deposit(){
+		
+		
+		
+		return 0;
+	}
+}
---
+++ src/atm/Test.java
@@ -0,0 +1,19 @@
+package atm;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*******************************");
+		System.out.println("*\t Welcome to ATM \t*");
+		System.out.println("*******************************\n\n");
+		System.out.println("1. deposit");
+		System.out.println("2. withdraw");
+		
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		
+	}
+}
