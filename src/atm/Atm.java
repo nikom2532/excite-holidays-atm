1b46dba283435040805d02c564caf661eafcf36b
Arming Huang <arming@buildthedot.com>
2015-01-09 15:54:28 +0700

    add test

--- src/atm/Atm.java
+++ src/atm/Atm.java
@@ -2,16 +2,43 @@ package atm;
 
 public class Atm {
 	
-	private double money = 0.0;
+	private long money = 0;
 	
-	public Atm(double cash){
-		this.money = cash;
+	public Atm(long cash){
+		this.setMoney(cash);
 	}
 	
-	public int deposit(){
-		
-		
-		
+	public int deposit(int deposit_money){
 		return 0;
+	}
+	
+	public int withdraw(int withdraw_money){
+		return 0;
+	}
+	
+	
+	public boolean checkMoney(long cost){
+		long note20 = 20;
+		long note50 = 50;
+		
+		if(cost % note20 != 0){
+			return false;
+		}
+		else if(cost % note50 != 0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	
+	
+	public long getMoney() {
+		return money;
+	}
+
+	public void setMoney(long money) {
+		this.money = money;
 	}
 }
--- src/atm/Test.java
+++ src/atm/Test.java
@@ -3,17 +3,59 @@ package atm;
 import java.util.Scanner;
 
 public class Test {
+	
+	static long money = 10000;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("*******************************");
-		System.out.println("*\t Welcome to ATM \t*");
-		System.out.println("*******************************\n\n");
-		System.out.println("1. deposit");
-		System.out.println("2. withdraw");
 		
 		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("*******************************");
+		System.out.println("*\t Welcome to ATM \t*");
+		System.out.println("*******************************\n");
+		
+		System.out.println("You have: " + money + " Dolar\n");
+		
+		System.out.println("1. deposit");
+		System.out.println("2. withdraw\n\n");
+		System.out.println("3. Exit\n\n");
+		System.out.print("please choose the choice: ");
+		
 		int i = sc.nextInt();
+		
+			
+		Atm obj = new Atm(money);
+		
+		if(i == 1){
+			System.out.print("How many money do you want to deposit: ");
+			int deposit_money = sc.nextInt();
+			
+			if(obj.checkMoney(deposit_money)){
+				obj.deposit(deposit_money);
+			}
+			else{
+				System.out.print("Cannot deposit. There are only 20 Note, and 50 Note");
+			}
+			
+		}
+		else if(i == 2){
+			System.out.print("How many money do you want to withdraw: ");
+			int withdraw_money = sc.nextInt();
+			
+			if(obj.checkMoney(withdraw_money)){
+				obj.deposit(withdraw_money);
+			}
+			else{
+				System.out.print("Cannot deposit. There are only 20 Note, and 50 Note");
+			}
+		}
+		else{
+			System.out.println("You choose incorrect choice\nExit.");
+		}
+		
+		
 		
 	}
 }
